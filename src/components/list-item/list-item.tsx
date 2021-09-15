4374c8d552901c6ea5dd98cd56bf4f080c6515e6
Szymon Błaszczak <szymon.blaszczak@iteo.com>
2021-09-15 14:26:51 +0200

    add remove and change status functions

--- src/components/list-item/list-item.tsx
+++ src/components/list-item/list-item.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import styles from './list-item.module.scss';
+import { TodoListContext } from '../../providers/todo-list.provider';
 
 type PropsType = {
   taskName: string;
@@ -7,10 +8,27 @@ type PropsType = {
 
 export const ListItem: React.FC<PropsType> = (props) => {
   const [value, setValue] = useState(props.taskName);
+  const { tasks, setTasks } = useContext(TodoListContext);
+
+  const handleDelete = (id: string) => {
+    // let tasksArr = [...tasks];
+    // tasksArr = tasksArr.filter(task=>task.id !== id)
+    // setTasks(tasks)
+  };
+
+  const changeStatus = (id: string) => {
+    // const tasksArr = [...tasks];
+    // tasksArr.forEach(task=> {
+    //     if(task.id === id) {
+    //         task.done = true;
+    //     }
+    // })
+    // setTasks(tasks)
+  };
 
   return (
     <div className={styles.root}>
-      <button>check</button>
+      <button onClick={() => changeStatus('1')}>check</button>
       <input
         className={styles.description}
         type="text"
@@ -18,7 +36,9 @@ export const ListItem: React.FC<PropsType> = (props) => {
         value={value}
         onChange={(e) => setValue(e.target.value)}
       />
-      <button className={styles.deleteButton}>x</button>
+      <button onClick={() => handleDelete('1')} className={styles.deleteButton}>
+        x
+      </button>
     </div>
   );
 };
